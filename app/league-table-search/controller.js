176c16967e17bc4266486ce2cc9efa8527cf2e7f
faiyazbits <m.faiyaz123@gmail.com>
2023-08-22 17:35:08 +0530

    league table search

--- app/league-table-search/controller.js
+++ app/league-table-search/controller.js
@@ -1,6 +1,10 @@
 import Controller from '@ember/controller';
 import { service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
 
 export default class LeagueTableSearchController extends Controller {
+  @service dealOptions;
 
+  @tracked
+  dealIdentifier = 'corporate';
 }
--- app/league-table-search/route.js
+++ app/league-table-search/route.js
@@ -2,5 +2,6 @@ import Route from '@ember/routing/route';
 import { service } from '@ember/service';
 
 export default class LeagueTableSearchRoute extends Route {
-
+  @service dealOptions;
+  @service store;
 }
