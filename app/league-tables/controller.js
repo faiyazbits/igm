884f0d07892d1689e80939d3dd38f6f70bc60374
Soofiya Binth Shajahan B <soofiyashajahan@gmail.com>
2023-11-05 11:46:48 +0530

    Commit

--- app/league-tables/controller.js
+++ app/league-tables/controller.js
@@ -1,6 +1,28 @@
-import Controller from '@ember/controller';
-import { service } from '@ember/service';
+// app/controllers/league-table/edit.js
 
-export default class LeagueTablesController extends Controller {
-  
+import Controller from '@ember/controller';
+import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
+
+export default class LeagueTableEditController extends Controller {
+  @service store;
+
+  @action
+  async saveLeagueTable() {
+    try {
+      const leagueTable = this.model;
+      await leagueTable.save();
+      this.transitionToRoute('league-table.show', leagueTable.id);
+    } catch (error) {
+      console.error('Error saving league table:', error);
+      // Handle error as needed
+    }
+  }
+
+  @action
+  async cancelEditing() {
+    const leagueTable = this.model;
+    leagueTable.rollbackAttributes();
+    this.transitionToRoute('league-table.show', leagueTable.id);
+  }
 }
--- app/league-tables/route.js
+++ app/league-tables/route.js
@@ -1,6 +1,12 @@
+// app/routes/league-table/edit.js
+
 import Route from '@ember/routing/route';
 import { service } from '@ember/service';
 
-export default class LeagueTablesRoute extends Route {
-  
+export default class LeagueTableEditRoute extends Route {
+  model(params) {
+    // Fetch and return the specific league table using the league_table_id parameter
+    return this.store.findRecord('league-table', params.league_table_id);
+ 
+  }
 }
